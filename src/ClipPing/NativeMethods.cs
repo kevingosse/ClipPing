ddb4528ed1585b11f066227ecff7e6cfb0603a71
Kevin Gosse <krix33@gmail.com>
2025-07-24 20:36:47 +0200

    Fix scaling for apps that aren't DPI aware

--- src/ClipPing/NativeMethods.cs
+++ src/ClipPing/NativeMethods.cs
@@ -14,13 +14,22 @@ internal static class NativeMethods
     public const int WS_EX_TRANSPARENT = 0x00000020;
     public const int WS_EX_LAYERED = 0x80000;
     public const int DWMWA_EXTENDED_FRAME_BOUNDS = 9;
+    public const uint MONITOR_DEFAULTTONEAREST = 2;
 
     [DllImport("user32.dll", SetLastError = true)]
     public static extern bool SetProcessDpiAwarenessContext(IntPtr dpiContext);
 
+    [DllImport("user32.dll")]
+    public static extern IntPtr GetWindowDpiAwarenessContext(IntPtr hwnd);
+
+    [DllImport("user32.dll")]
+    public static extern DPI_AWARENESS GetAwarenessFromDpiAwarenessContext(IntPtr value);
 
     [DllImport("user32.dll")]
     public static extern uint GetDpiForWindow(IntPtr hwnd);
+
+    [DllImport("shcore.dll")]
+    public static extern int GetDpiForMonitor(IntPtr hmonitor, MONITOR_DPI_TYPE dpiType, out uint dpiX, out uint dpiY);
 
     [DllImport("dwmapi.dll")]
     public static extern int DwmGetWindowAttribute(
@@ -38,8 +47,25 @@ internal static class NativeMethods
     [DllImport("user32.dll")]
     public static extern IntPtr GetForegroundWindow();
 
+    [DllImport("user32.dll")]
+    public static extern IntPtr MonitorFromWindow(IntPtr hwnd, uint dwFlags);
+
     [DllImport("user32.dll", SetLastError = true)]
     public static extern bool AddClipboardFormatListener(IntPtr hwnd);
+
+    public enum MONITOR_DPI_TYPE
+    {
+        MDT_EFFECTIVE_DPI = 0,
+        MDT_ANGULAR_DPI = 1,
+        MDT_RAW_DPI = 2,
+    }
+
+    public enum DPI_AWARENESS
+    {
+        UNAWARE = 0,
+        SYSTEM_AWARE = 1,
+        PER_MONITOR_AWARE = 2
+    }
 
     [StructLayout(LayoutKind.Sequential)]
     public readonly struct RECT(int left, int top, int right, int bottom)
